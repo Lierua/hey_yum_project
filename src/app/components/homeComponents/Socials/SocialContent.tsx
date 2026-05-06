f4fbcd55159515b0fbb12f400980627a454c0dc9
Oliver AL Bengtsson <oliverbengtsson67@gmail.com>
2026-05-06 12:12:57 +0200

    ++

--- src/app/components/homeComponents/Socials/SocialContent.tsx
+++ src/app/components/homeComponents/Socials/SocialContent.tsx
@@ -4,21 +4,23 @@ import SocialText from "./SocialsText";
 const SocialContent = () => {
   return (
     <div className="grid grid-rows-2 gap-[60] max-h-[1020]">
-      <div className="grid grid-cols-[1fr_1fr_2fr] gap-[40]">
-        <div className="relative w-full h-[484]">
+      <div className="grid grid-cols-[1fr_1fr_2fr] gap-[20]">
+        <div className="relative w-full h-[440]">
           <Image
             loading="eager"
             src="/assets/images/home/Social-img-1.png"
             alt="Social Media image"
+            sizes="(max-width: 768px) 100vw, 50vw"
             fill
             className="object-cover h-auto w-auto"
           />
         </div>
-        <div className="relative w-full h-[484]">
+        <div className="relative w-full h-[440]">
           <Image
             loading="eager"
             src="/assets/images/home/Social-img-2.png"
             alt="Social Media image"
+            sizes="(max-width: 768px) 100vw, 50vw"
             fill
             className="object-cover h-auto w-auto"
           />
@@ -27,23 +29,25 @@ const SocialContent = () => {
           <SocialText />
         </div>
       </div>
-      <div className="grid grid-cols-[1fr_3fr_1fr] gap-[40]">
+      <div className="grid grid-cols-[1fr_3fr_1fr] gap-[20]">
         <div></div>
-        <div className="relative w-full h-[484] ml-auto">
+        <div className="relative w-full h-[440] ml-auto">
           <Image
             loading="eager"
             src="/assets/images/home/Social-img-3.png"
             alt="Social Media image"
+            sizes="(max-width: 768px) 100vw, 50vw"
             fill
             className="object-cover w-auto h-auto"
           />
         </div>
 
-        <div className="relative w-full h-[484]">
+        <div className="relative w-full h-[440]">
           <Image
             loading="eager"
             src="/assets/images/home/Social-img-4.png"
             alt="Social Media image"
+            sizes="(max-width: 768px) 100vw, 50vw"
             fill
             className="object-cover h-auto w-auto"
           />
